b0ad041363ec0c9bc2085b066815ee12401ff3cc
chandu98 <chandrakiran1221@gmail.com>
2021-07-21 11:31:27 +0530

    Pubsub features added

---
+++ src/main/java/com/ftd/example/SpringBootMongoDBApplication/pubsub/PubsubConfig.java
@@ -0,0 +1,66 @@
+package com.ftd.example.SpringBootMongoDBApplication.pubsub;
+
+import com.ftd.example.SpringBootMongoDBApplication.SpringBootMongoDbApplication;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.integration.AckMode;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
+import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
+import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PubsubConfig {
+
+    private static final Log LOGGER = LogFactory.getLog(SpringBootMongoDbApplication.class);
+
+    @Bean
+    public PubSubInboundChannelAdapter messageChannelAdapter(
+            @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
+            PubSubTemplate pubSubTemplate) {
+        PubSubInboundChannelAdapter adapter =
+                new PubSubInboundChannelAdapter(pubSubTemplate, "newSubscription");
+        adapter.setOutputChannel(inputChannel);
+        adapter.setAckMode(AckMode.MANUAL);
+
+        return adapter;
+    }
+
+    @Bean
+    public MessageChannel pubsubInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "pubsubInputChannel")
+    public MessageHandler messageReceiver() {
+        return message -> {
+            LOGGER.info("Message arrived! Payload: " + new String((byte[]) message.getPayload()));
+            BasicAcknowledgeablePubsubMessage originalMessage =
+                    message.getHeaders().get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage.class);
+
+            originalMessage.ack();
+        };
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "pubsubOutputChannel")
+    public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
+        return new PubSubMessageHandler(pubsubTemplate, "newTopic");
+    }
+
+    @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+    public interface PubsubOutboundGateway {
+
+        void sendToPubsub(String text);
+    }
+}
